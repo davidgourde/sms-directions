74baec08c49fc6289b0942cf809bd03cdce00ccd
David Gourde <gourde.david@outlook.com>
2018-02-28 21:06:42 -0500

    Added Google Maps directions parsing

--- sms_directions/app.py
+++ sms_directions/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import flask_restful as rest
 
 from sms_directions.directions import get_directions
+from sms_directions.sms import send_sms
 
 
 def init_rest_api(app):
@@ -9,14 +10,15 @@ def init_rest_api(app):
 
     class GetDirections(rest.Resource):
         def get(self):
-            try:
-                origin, destination, mode = request.args.get('text').split(';')
-                print('argsText = ' + request.args.get('text'))
-            except Exception as e:
-                print(e)
-                origin, destination, mode = 'Laval', 'Montreal', 'transit'
-            finally:
-                return get_directions(origin, destination, mode), 200
+            args = request.args.get('text').split(';')
+            if len(args) == 2:
+                origin, destination = args
+                mode = 'driving'
+            elif len(args) == 3:
+                origin, destination, mode = args
+            else:
+                send_sms('La syntaxe correcte est: origin;destination;transit où ;transit est optionnel.')
+            return get_directions(origin, destination, mode), 200
 
     api.add_resource(GetDirections, '/')
 
--- sms_directions/directions.py
+++ sms_directions/directions.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 import googlemaps
+from bs4 import BeautifulSoup
 
 from sms_directions.config import GOOGLE_MAPS_DIRECTIONS_API_KEY
 from sms_directions.sms import send_sms
+
 
 gmaps = googlemaps.Client(key=GOOGLE_MAPS_DIRECTIONS_API_KEY) if GOOGLE_MAPS_DIRECTIONS_API_KEY else None
 
@@ -17,4 +19,18 @@ def get_directions(origin, destination, mode):
                                          mode=mode,
                                          departure_time=now,
                                          language='fr')
-    send_sms(directions_result)
+
+    result = ""
+    i = 1
+    leg = directions_result[0]['legs'][0]
+    result += 'Départ: ' + leg['start_address'] + '\n'
+    result += 'Arrivée: ' + leg['end_address'] + '\n\n'
+
+    for step in leg['steps']:
+        html_instructions = step['html_instructions']
+        soup = BeautifulSoup(html_instructions)
+        instructions = soup.get_text()
+        result += "{}. {}".format(i, instructions) + '\n\n'
+        i = i + 1
+
+    send_sms(result)
